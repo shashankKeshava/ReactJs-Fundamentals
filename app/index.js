21c9b8057e788aa5d1cf62497d1da2b755ddde02
Shashank <shashank.keshava@bookmyshow.com>
2016-03-23 13:25:18 +0530

    added preset in react

--- app/index.js
+++ app/index.js
@@ -5,7 +5,7 @@ var USER_DATA = {
     name: 'Shashank K',
     username: 'Kexcaliber',
     image: 'http://dayt.se/uploads/full/Vikings-TMNS.png'
-}
+};
 
 var React = require('react');
 var ReactDOM = require('react-dom');
@@ -17,39 +17,36 @@ Small
 Testable
 */
 
+var HelloWorld = React.createClass({
+    render: function() {
+        return (<div> Hello World ReactJS!!!! </div>)
+    }
+});
+
 
 var Container = React.createClass({
     render: function() {
-        return (
-            <Wrapper>
-        <Welcome user={USER_DATA}/> 
-        <Hello/>
-      </Wrapper>
-        )
+        return (<Wrapper>
+            <Welcome user = { USER_DATA }/>  <Hello/>
+            </Wrapper>)
     }
 });
 
-var Wrapper = React.createClass({
-    render: function() {
-        return (
-            <div style={{backgroundColor: 'yellow'}}>
-        <h1> ReactJS Fundamentals </h1>
-        {this.props.children}
-      </div>
-        )
-    }
-});
+
 
 var Welcome = React.createClass({
     render: function() {
-        return <div>Welcome {this.props.user.name}!</div>
+        return <div> Welcome { this.props.user.name }! </div>
     }
 });
 
 var Hello = React.createClass({
     render: function() {
-        return <div>Hello ReactJS Program!</div>
+        return <div> Hello ReactJS Program! </div>
     }
-})
+});
 
-ReactDOM.render(<Container />, document.getElementById('app'));
+
+//ReactDOM.render(<Container />, document.getElementById('app'));
+
+ReactDOM.render(<HelloWorld />, document.getElementById('app'));
