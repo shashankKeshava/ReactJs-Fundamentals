1fe85a6c699d59803fca25a70df6b150ae87a19b
Shashank <shashank.keshava@bookmyshow.com>
2016-05-09 16:37:59 +0530

    Routes Stateless functions PropTypes Methods implemented

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,10 +1,25 @@
 var React = require('react');
+var ReactRouter = require('react-router');
+var Link = ReactRouter.Link;
+var transparentBg = require('../styles').transparentBg;
 
 var Home = React.createClass({
     render: function() {
         return (
-            <div>
-              Hello from Home!!!!!
+            <div
+                 className="jumbotron col-sm-12 text-center"
+                 style={ transparentBg }>
+              <h1>GitHub Battle</h1>
+              <p className='lead'>
+                What even in jquery?
+              </p>
+              <Link to='/playerOne'>
+              <button
+                      type='button'
+                      className='btn btn-lg btn-success'>
+                Get Started
+              </button>
+              </Link>
             </div>
         )
     }
--- app/config/routes.js
+++ app/config/routes.js
@@ -4,18 +4,27 @@ var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var IndexRoute = ReactRouter.IndexRoute;
+var hashHistory = ReactRouter.hashHistory;
 
 var Main = require('../components/Main');
 var Home = require('../components/Home');
 
+var PromptContainer = require('../containers/PromptContainer');
+
 var routes = (
-<Router>
+<Router history={ hashHistory }>
   <Route
          path='/'
          component={ Main }>
-    <IndexRoute
-                path='/home'
-                component={ Home } />
+    <IndexRoute component={ Home } />
+    <Route
+           path='playerOne'
+           header='Player One'
+           component={ PromptContainer } />
+    <Route
+           path='playerTwo/:playerOne'
+           header='Player Two'
+           component={ PromptContainer } />
   </Route>
 </Router>
 );
